0e66e8fec9778acc1b33efa5539e3a8c6ba28a8f
Marco Rojo <mrmarcondes@gmail.com>
2013-11-03 14:41:34 -0200

    correcao de formula

--- app/models/match.rb
+++ app/models/match.rb
@@ -17,11 +17,12 @@ class Match
   end
 
   def self.total_matches(player)
-    total_matches = won_matches(player) + lost_matches(player)
+    total_matches = self.won_matches(player) + self.lost_matches(player)
   end
 
   def self.percentage_of_wins(player)
-    percentage_of_wins = (won_matches(player) / total_matches(player)) * 100
+    percentage_of_wins = (self.won_matches(player).to_f / self.total_matches(player).to_f) * 100.0
+    return percentage_of_wins.round(2)
   end
 
 
